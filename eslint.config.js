d586916d4f9268d2edd65249c040a5badfbd668b
max <max@maxlabs.se>
2025-10-27 11:27:18 +0100

    chore: resolve project correctly in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,10 @@ export default [
   {
     languageOptions: {
       parserOptions: {
-        project: path.resolve(__dirname, "tsconfig.json"),
+        project: [
+          path.resolve(__dirname, "tsconfig.json"),
+          path.resolve(__dirname, "apps/backend/tsconfig.json"),
+        ],
         tsconfigRootDir: path.resolve(__dirname),
       },
       globals: {

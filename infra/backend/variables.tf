f0e9a37cfe0c3efdcc8fc9656ec110d17364c079
max <max@maxlabs.se>
2025-10-28 07:55:04 +0100

    infra: deploy sm

--- infra/backend/variables.tf
+++ infra/backend/variables.tf
@@ -8,3 +8,37 @@ variable "env" {
   type        = string
   default     = "dev"
 }
+
+variable "google_client_id" {
+  description = "Google OAuth Client ID"
+  sensitive   = true
+  type        = string
+}
+
+variable "google_client_secret" {
+  description = "Google OAuth Client Secret"
+  sensitive   = true
+  type        = string
+}
+
+variable "google_redirect_uri" {
+  description = "Google OAuth Redirect URI"
+  type        = string
+}
+
+variable "openai_api_key" {
+  description = "OpenAI API Key"
+  sensitive   = true
+  type        = string
+}
+
+variable "jwt_private_key" {
+  description = "JWT Private Key"
+  sensitive   = true
+  type        = string
+}
+
+variable "jwt_public_key" {
+  description = "JWT Public Key"
+  type        = string
+}

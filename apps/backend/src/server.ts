d8cdc5737ae0db4d1792812202d759860c7c7218
max <max@maxlabs.se>
2025-10-28 14:01:21 +0100

    chore: test sm

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,9 +1,10 @@
-import { createApp } from './app.js';
-import config from './config/environment.js';
+import { createApp } from "./app.js";
+import config from "./config/environment.js";
 
 const app = createApp();
 
 app.listen(config.port, () => {
-  console.log(`Server running at http://${config.baseUrl}`);
+  console.log(`Server running at ${config.baseUrl}:${config.port}`);
   console.log(`Environment: ${config.env}`);
+  console.log(`Public Key: ${config.jwtPublicKey}`);
 });

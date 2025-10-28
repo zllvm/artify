c50fcdc828dd0ecfcd6a126a4ebccc7dbc122a84
max <max@maxlabs.se>
2025-10-28 12:56:19 +0100

    infra: deploy ecs

---
+++ infra/backend/ecs_service.tf
@@ -0,0 +1,101 @@
+data "aws_cloudformation_export" "vpc" {
+  name = "core-vpc"
+}
+
+data "aws_cloudformation_export" "ecs_cluster" {
+  name = "ecs-common-cluster"
+}
+
+data "aws_cloudformation_export" "ecs_subnet" {
+  name = "core-vpc-public-subnet-a"
+}
+
+data "aws_cloudformation_export" "ecs_sg" {
+  name = "core-vpc-sg-basic-fargate"
+}
+
+data "aws_cloudformation_export" "elb_sg" {
+  name = "ecs-common-lb-sg"
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${local.service_name}-ecs-tasks-sg"
+  description = "Allow inbound traffic to ECS tasks"
+  vpc_id      = data.aws_cloudformation_export.vpc.value
+
+  # Inbound: ALB -> ECS container port
+  ingress {
+    description = "Allow inbound from ALB"
+    from_port   = var.container_port
+    to_port     = var.container_port
+    protocol    = "tcp"
+    security_groups = [data.aws_cloudformation_export.elb_sg.value]
+  }
+
+  # Outbound: allow all
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "${var.service_name}-tg" # max 32 chars
+  port        = var.container_port
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = data.aws_cloudformation_export.vpc.value
+
+  health_check {
+    path = var.health_endpoint
+    port = var.container_port
+  }
+}
+
+resource "aws_lb_listener_rule" "app" {
+  listener_arn = data.aws_cloudformation_export.elb_listener.value
+  priority     = 3
+
+  condition {
+    host_header {
+      values = [var.api_domain_name]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+resource "aws_ecs_service" "main" {
+  name            = local.service_name
+  cluster         = data.aws_cloudformation_export.ecs_cluster.value
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [data.aws_cloudformation_export.ecs_subnet.value]
+    security_groups = [data.aws_cloudformation_export.ecs_sg.value, aws_security_group.ecs_tasks.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = local.service_name
+    container_port   = var.container_port
+  }
+
+  depends_on = [ aws_lb_listener_rule.app, aws_lb_target_group.app ]
+  
+  tags = local.tags
+}
+
+
+output "ecs_service_name" {
+  value = aws_ecs_service.main.name
+}
---
+++ infra/backend/ecs_task.tf
@@ -0,0 +1,140 @@
+data "aws_region" "current" {}
+
+resource "aws_cloudwatch_log_group" "main" {
+  name              = var.service_name
+  retention_in_days = 30
+
+  tags = local.tags
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${local.service_name}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Sid    = "ecs"
+        Principal = {
+          Service = ["ecs-tasks.amazonaws.com"]
+        }
+        Action = ["sts:AssumeRole"]
+      }
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_policy" "metrics_policy" {
+  name        = "${local.service_name}-metrics-policy"
+  description = "Policy for CloudWatch metrics"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["cloudwatch:PutMetricData"]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${local.service_name}-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Sid    = "ecs"
+        Principal = {
+          Service = ["ecs-tasks.amazonaws.com"]
+        }
+        Action = ["sts:AssumeRole"]
+      }
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "metrics_policy_attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.metrics_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_secret_access" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = module.json_secret.read_policy_arn
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = "${local.service_name}"
+  network_mode              = "awsvpc"
+  requires_compatibilities  = ["FARGATE"]
+  cpu                       = var.cpu
+  memory                    = var.memory
+  execution_role_arn        = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn             = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.service_name
+      image     = var.container_image
+      essential = true
+      portMappings = [{
+        containerPort = var.container_port
+      }]
+      environment = [
+        {
+          name  = "NODE_ENV"
+          value = var.env == "prod" ? "production" : var.env
+        },
+        {
+          name  = "PORT"
+          value = tostring(var.container_port)
+        },
+        {
+          name  = "BASE_URL"
+          value = var.base_url
+        },
+        {
+          name  = "FRONTEND_URL"
+          value = var.frontend_url
+        }
+      ]
+      secrets = [
+        for key in local.secret_keys : {
+          name      = key
+          valueFrom = "${module.json_secret.secret_arn}:${key}::"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.main.name
+          awslogs-region        = data.aws_region.current.region
+          awslogs-stream-prefix = local.service_name
+        }
+      }
+    }
+  ])
+
+  tags = local.tags
+}
+
+
+output "ecs_task_definition_arn" {
+  value = aws_ecs_task_definition.main.arn
+}

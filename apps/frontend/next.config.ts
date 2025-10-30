0b0caced01c2297ac4ad3faa7694aa1d098512e2
max <max@maxlabs.se>
2025-10-30 09:35:53 +0100

    fix(ci): build frontend correctly

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // allowedDevOrigins: ["https://artify.maxlabs.se"],
+  output: "standalone",
 };
 
 export default nextConfig;

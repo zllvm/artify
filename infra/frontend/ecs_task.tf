f7e2c17881bb995b129a33b72bfdd67fb29cffa2
max <max@maxlabs.se>
2025-10-30 10:10:43 +0100

    ci: add backend port

--- infra/frontend/ecs_task.tf
+++ infra/frontend/ecs_task.tf
@@ -90,6 +90,10 @@ resource "aws_ecs_task_definition" "main" {
           value = var.env == "prod" ? "production" : var.env
         },
         {
+          name  = "PORT"
+          value = tostring(var.container_port)
+        },
+        {
           name  = "API_URL"
           value = var.backend_url
         },

5dc4500e720c86264072df7385955f320adbdc25
max <max@maxlabs.se>
2025-11-19 10:08:30 +0100

    fix(ci): define domain correctly

--- infra/frontend/locals.tf
+++ infra/frontend/locals.tf
@@ -3,7 +3,8 @@ locals {
 
   service_name = "${var.service_name}-${var.app_name}"
   env_service_name = "${var.env}-${local.service_name}"
-  domain = "${var.service_name}.${var.domain_name}"
+  domain = var.domain_name
+  # domain = "${var.service_name}.${var.domain_name}"
 
    secret_keys = [
     "SENTRY_AUTH_TOKEN","SERVICE_CLIENT_ID","SERVICE_CLIENT_SECRET"

406e8ec8da208bb7ba68553b39a7592f66ca0860
max <max@maxlabs.se>
2025-11-20 11:12:49 +0100

    fix(frontend): sentry

--- apps/frontend/instrumentation.ts
+++ apps/frontend/instrumentation.ts
@@ -3,10 +3,10 @@ import * as Sentry from "@sentry/nextjs";
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
-  } else if (process.env.NEXT_RUNTIME === "edge") {
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
     await import("./sentry.edge.config");
-  } else {
-    await import("./sentry.client.config");
   }
 }
 

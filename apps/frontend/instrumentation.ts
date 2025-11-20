cb094493c725b2ea62c17f7fdd1dc47c38157f37
max <max@maxlabs.se>
2025-11-20 10:34:07 +0100

    fix: sentry

--- apps/frontend/instrumentation.ts
+++ apps/frontend/instrumentation.ts
@@ -3,8 +3,7 @@ import * as Sentry from "@sentry/nextjs";
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
-  }
-  if (process.env.NEXT_RUNTIME === "edge") {
+  } else if (process.env.NEXT_RUNTIME === "edge") {
     await import("./sentry.edge.config");
   } else {
     await import("./sentry.client.config");
--- apps/frontend/sentry.client.config.ts
+++ apps/frontend/sentry.client.config.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  // debug: true,
+  debug: false,
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
